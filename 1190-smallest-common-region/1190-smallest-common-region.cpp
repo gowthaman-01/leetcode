115c740458dbb38f81cdf3db34e73d5a9e481402
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2023-10-08 08:36:00 +0800

    Time: 75 ms (57.5%), Space: 41 MB (25%) - LeetHub

--- 1190-smallest-common-region/1190-smallest-common-region.cpp
+++ 1190-smallest-common-region/1190-smallest-common-region.cpp
@@ -1,50 +1,20 @@
-struct Region {
-    string name = "";
-    string bigger = "";
-};
-
 class Solution {
 public:
     string findSmallestRegion(vector<vector<string>>& regions, string region1, string region2) {
-        unordered_map<string, Region>map; 
-        for (vector<string> regionList: regions) {
-            string biggerRegionName = regionList[0];
-            Region biggerRegion; 
-            if (map.count(biggerRegionName)) {
-                biggerRegion = map[biggerRegionName];
-            } else {
-                biggerRegion.name = biggerRegionName;
-                map[biggerRegionName] = biggerRegion;
-            }
-            for (int i = 1; i < regionList.size(); i++) {
-                string smallerRegionName = regionList[i]; 
-                Region smallerRegion; 
-                if (map.count(smallerRegionName)) {
-                    smallerRegion = map[smallerRegionName];
-                } else {
-                    smallerRegion.name = smallerRegionName; 
-                }
-                smallerRegion.bigger = biggerRegionName;
-                map[smallerRegionName] = smallerRegion; 
+        unordered_map<string, string> map; 
+        for (vector<string> rList: regions) {
+            for (int i = 1; i < rList.size(); i++) {
+                map[rList[i]] = rList[0];
             }
         }
-
-        unordered_set<string>visited;
-        Region r1 = map[region1]; Region r2 = map[region2]; 
-        visited.insert(r1.name); 
-        visited.insert(r2.name);
-        while (true) {
-            if (map.count(r1.bigger)) {
-                r1 = map[r1.bigger];
-                if (visited.count(r1.name)) return r1.name; 
-                visited.insert(r1.name); 
-            }
-            if (map.count(r2.bigger)) {
-                r2 = map[r2.bigger]; 
-                if (visited.count(r2.name)) return r2.name; 
-                visited.insert(r2.name);
-            }
+        unordered_set<string>bigger; 
+        while(region1 != "") {
+            bigger.insert(region1); 
+            region1 = map[region1];
+        } 
+        while(!bigger.count(region2)) {
+            region2 = map[region2];
         }
-        return "";
+        return region2; 
     }
 };
