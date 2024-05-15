fd591eb3fa819018bb12c92aeebe0e42689e251b
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-05-15 18:13:20 +0800

    Time: 42 ms (6.83%), Space: 9.6 MB (32.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        unordered_map<char, int> charMap; 
+        for (char c: s) {
+            charMap[c]++;
+        }
+        for (char c: t) {
+            cout << charMap[c] << endl;
+            if (!charMap.count(c) || charMap[c] == 0) {
+                return false;
+            } else {
+                charMap[c]--;
+            }
+        }
+        return true;
+    }
+};
