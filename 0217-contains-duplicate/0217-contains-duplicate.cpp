292c9d93bbd9d445e39ec31817b0761da020edf5
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-12-19 09:06:23 +0800

    Time: 32 ms (56.4%), Space: 73.4 MB (53.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::unordered_set<int>unique_nums;
+        std::unordered_set<int> unique_nums;
+        
         for (int num: nums) {
             if (unique_nums.count(num)) {
                 return true;
             }
             unique_nums.insert(num);
         }
+
         return false;
     }
 };
