e120177891aa042febf905f56356e67e8e76806b
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-12-19 10:14:10 +0800

    Time: 6 ms (30.27%), Space: 19.7 MB (6.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        std::unordered_map<int, int> freq_map;
+        std::unordered_map<int, int> freq;
         for (int num: nums) {
-            freq_map[num]++;
+            freq[num]++;
         }
 
-        std::vector<std::vector<int>> buckets(nums.size() + 1);
-        for (auto& [num, freq]: freq_map) {
-            buckets[freq].push_back(num);
+        std::vector<std::vector<int>> bucket(nums.size() + 1);
+        for (const auto& [num, count]: freq) {
+            bucket[count].push_back(num);
         }
 
         std::vector<int> res;
-        for (int i = nums.size(); i >= 0; i--) {
+        for (int i = nums.size(); i > 0; i--) {
+            for (int num: bucket[i]) {
+                res.push_back(num);
+                k--;
+            }
             if (k == 0) {
                 return res;
             }
-
-            for (int n: buckets[i]) {
-                res.push_back(n);
-                k--;
-            }
         }
-
+        
         return res;
     }
 };
