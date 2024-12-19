60299a249917dcefd8223c525eb39c22fae1c247
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-12-19 09:45:51 +0800

    Time: 61 ms (14.58%), Space: 28.9 MB (18.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        std::unordered_map<string, vector<string>> anagrams_map;
-
-        for (const std::string& str: strs) {
-            int count[26] = {0};
-            for (char c: str) {
-                count[c - 'a']++;
-            }
-
-            std::string key = "";
-            for (int i = 0; i < 26; i++) {
-                key += std::to_string(count[i]) + ".";
-            }
-
-            anagrams_map[key].push_back(str);
+    std::string key_from_string(const std::string& str) {
+        std::array<int, 26> char_count = {0};
+        for (char c: str) {
+            char_count[c - 'a']++;
         }
 
-        std::vector<std::vector<string>> res;
-        for (const auto& elem: anagrams_map) {
-            res.push_back(std::move(elem.second));
+        std::string key = "";
+        for (int count: char_count) {
+            key += std::to_string(count) + "/";
+        }
+
+        return key;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> anagrams;
+
+        for (const std::string& str: strs) {
+            anagrams[key_from_string(str)].push_back(str);
+        }
+
+        std::vector<std::vector<std::string>> res;
+        for (const auto& elem: anagrams) {
+            res.push_back(elem.second);
         }
 
         return res;
