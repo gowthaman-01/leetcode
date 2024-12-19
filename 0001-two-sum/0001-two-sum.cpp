553b9d5259dc5484ae4f2ac38f1bcc827b06d19d
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-12-19 09:26:32 +0800

    Time: 0 ms (100%), Space: 14.9 MB (19.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> num_to_index(nums.size());
-        
+        std::unordered_map<int, int> num_to_index;
+
         for (int i = 0; i < nums.size(); i++) {
-            int other = target - nums[i];
-            if (num_to_index.count(other)) {
-                return {num_to_index[other], i};
-            } else {
-                num_to_index[nums[i]] = i;
+            int complement = target - nums[i];
+            if (num_to_index.count(complement)) {
+                return {i, num_to_index[complement]};
             }
+            num_to_index[nums[i]] = i;
         }
 
         return {};
