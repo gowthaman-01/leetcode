8ad690c2a00f7c2328902c80d586f9c70744644a
Gowthaman Aravindan <77627894+gowthaman-01@users.noreply.github.com>
2024-12-24 20:22:08 +0800

    Time: 2 ms (62.39%), Space: 17.9 MB (30.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    void findCombinations(std::vector<int>& candidates, int target, std::vector<int>& current, int current_sum, int i, std::vector<std::vector<int>>& combinations) {
-        if (current_sum > target || i == candidates.size()) return;
+    void findCombinations(std::vector<int>& candidates, int target,
+                          std::vector<int>& current, int current_sum, int i,
+                          std::vector<std::vector<int>>& combinations) {
+        if (current_sum > target || i == candidates.size())
+            return;
         if (current_sum == target) {
             combinations.emplace_back(current);
             return;
         }
 
-        findCombinations(candidates, target, current, current_sum, i + 1, combinations);
+        findCombinations(candidates, target, current, current_sum, i + 1,
+                         combinations);
 
         current.push_back(candidates[i]);
-        findCombinations(candidates, target, current, current_sum + candidates[i], i, combinations);
+        findCombinations(candidates, target, current,
+                         current_sum + candidates[i], i, combinations);
         current.pop_back();
     }
 
